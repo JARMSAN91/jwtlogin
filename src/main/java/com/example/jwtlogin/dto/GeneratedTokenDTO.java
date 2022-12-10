b4f525841b31e03262a40b12f6eadbd2c9079f7b
Julio A <jarmulao@gmail.com>
2022-12-10 17:58:57 +0100

    Added Dto objects

---
+++ src/main/java/com/example/jwtlogin/dto/GeneratedTokenDTO.java
@@ -0,0 +1,11 @@
+package com.example.jwtlogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GeneratedTokenDTO {
+
+    private String accessToken;
+}
---
+++ src/main/java/com/example/jwtlogin/dto/UserDataDTO.java
@@ -0,0 +1,13 @@
+package com.example.jwtlogin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDataDTO {
+
+    private String id;
+    private String email;
+
+}
