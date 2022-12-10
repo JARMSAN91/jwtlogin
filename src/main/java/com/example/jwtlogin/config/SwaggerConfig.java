4fe8a2db16a0995b0b78daab12c03a3c5c236146
Julio A <jarmulao@gmail.com>
2022-12-10 17:59:22 +0100

    Added swagger basic info and security configuration

---
+++ src/main/java/com/example/jwtlogin/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.example.jwtlogin.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+
+public class SwaggerConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(apiInfo().version("3.0.1"));
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Imbric login with JSON Web Token Authentication API")
+                .description("Simple jwt login example")
+                .version("0.0.1")
+                .contact(apiContact());
+    }
+
+
+
+    private Contact apiContact() {
+        return new Contact()
+                .name("Julio Alberto Raposo")
+                .email("julioalberto91@gmail.com");
+    }
+
+}
